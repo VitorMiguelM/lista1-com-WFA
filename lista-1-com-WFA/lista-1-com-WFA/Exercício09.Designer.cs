f0d74d4559060b45632fe74e2a20231b2e0d510c
Vitor Miguel <38699921+VitorMiguelM@users.noreply.github.com>
2018-05-17 16:36:57 -0300

    Finalização do design do Exercício09

--- lista-1-com-WFA/lista-1-com-WFA/Exercício09.Designer.cs
+++ lista-1-com-WFA/lista-1-com-WFA/Exercício09.Designer.cs
@@ -28,11 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Numero02 = new System.Windows.Forms.TextBox();
+            this.txtNumero01 = new System.Windows.Forms.TextBox();
+            this.btnResultado = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Número 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(375, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "De os dois numeros e eles trocarão de valor entre si:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(227, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Número 2";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(213, 94);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(119, 56);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Numero02
+            // 
+            this.Numero02.Location = new System.Drawing.Point(323, 55);
+            this.Numero02.Name = "Numero02";
+            this.Numero02.Size = new System.Drawing.Size(100, 20);
+            this.Numero02.TabIndex = 4;
+            // 
+            // txtNumero01
+            // 
+            this.txtNumero01.Location = new System.Drawing.Point(121, 55);
+            this.txtNumero01.Name = "txtNumero01";
+            this.txtNumero01.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero01.TabIndex = 5;
+            // 
+            // btnResultado
+            // 
+            this.btnResultado.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.btnResultado.Location = new System.Drawing.Point(113, 100);
+            this.btnResultado.Name = "btnResultado";
+            this.btnResultado.Size = new System.Drawing.Size(94, 50);
+            this.btnResultado.TabIndex = 6;
+            this.btnResultado.Text = "Resultado";
+            this.btnResultado.UseVisualStyleBackColor = false;
+            // 
+            // Exercício09
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(431, 156);
+            this.Controls.Add(this.btnResultado);
+            this.Controls.Add(this.txtNumero01);
+            this.Controls.Add(this.Numero02);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Exercício09";
             this.Text = "Exercício09";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox Numero02;
+        private System.Windows.Forms.TextBox txtNumero01;
+        private System.Windows.Forms.Button btnResultado;
     }
 }
